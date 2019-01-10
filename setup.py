0e7f8e68b078a687226cdbad756364d0a9290c62
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-01-10 23:39:40 +0000

    Finish configuring packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='nugetjanitor',
     version='1.0.0',
-    packages=['nugetjanitor'],
+    packages=setuptools.find_packages(),
+    install_requires=['semver>=2.8.1'],
     url='https://github.com/ThetaSinner/nuget-janitor',
     license=' GPL-3.0',
     author='ThetaSinner',
-    author_email='',
+    author_email='greatdjonfire@hotmail.co.uk',
     scripts=['bin/nugetjanitor'],
     description='Helper for cleaning a NuGet package repository on a file share.'
 )
